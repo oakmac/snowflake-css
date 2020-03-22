c33436d68946e8638575c84bb6865c5d271550bc
Chris Oakman <chris@oakmac.com>
2020-03-22 17:27:20 -0500

    build command works

--- src-cljs/snowflake_css/cli/core.cljs
+++ src-cljs/snowflake_css/cli/core.cljs
@@ -29,9 +29,28 @@
 
 (def possibles-regex #"([a-zA-Z0-9]+-){1,}([abcdef0-9]){5}")
 
+(defn- read-file-sync! [filename]
+  (.readFileSync fs filename "utf8"))
+
+(defn- write-file-sync! [filename file-contents]
+  (.writeFileSync fs filename file-contents "utf8"))
+
+(defn- get-snowflake-classes-from-str
+  "returns a set of snowflake classes from a string"
+  [a-string]
+  (->> (re-seq possibles-regex a-string)
+       (map first)
+       (filter snowflake-class?)
+       set))
+
+(assert (get-snowflake-classes-from-str "fizzle-44ebc") #{"fizzle-44ebc"})
+(assert (get-snowflake-classes-from-str ".fizzle-44ebc a") #{"fizzle-44ebc"})
+(assert (get-snowflake-classes-from-str ".fizzle-44ebc a, .foo-56bec") #{"fizzle-44ebc" "foo-56bec"})
+(assert (get-snowflake-classes-from-str "<div class=\"fizzle-44ebc\">") #{"fizzle-44ebc"})
+
 (defn- get-snowflake-classes-from-file [file]
   ; (timbre/info "Reading" file "…")
-  (let [file-contents (.readFileSync fs file "utf8")
+  (let [file-contents (read-file-sync! file)
         hashes (->> (re-seq possibles-regex file-contents)
                     (map first)
                     (filter snowflake-class?)
@@ -40,14 +59,17 @@
     hashes))
 
 ;; TODO: need to throw an error here if css-content cannot be parsed
-(defn- remove-classes-from-css [css-content classes-to-remove]
+(defn- remove-flakes-from-css [css-content flakes-to-keep-around]
   (let [root-node (.parse postcss css-content)]
     (.walkRules root-node
-      (fn [node]
-        (let [selector (oget node "selector")]
-          (doseq [c classes-to-remove] ;; <-- FIXME: this is slow; O(n^2)
-            (when (str/starts-with? selector (str "." c))
-              (.remove node))))))
+      (fn [js-node]
+        (let [selector (oget js-node "selector")
+              flakes-in-selector (get-snowflake-classes-from-str selector)]
+          ;; 1) only look at rulesets that contain snowflake classes
+          ;; 2) remove any ruleset that does not exist in flakes-to-keep-around
+          (when (and (not (empty? flakes-in-selector))
+                     (empty? (set/intersection flakes-in-selector flakes-to-keep-around)))
+            (.remove js-node)))))
     (oget (.toResult root-node) "css")))
 
 (defn- glob-sync
@@ -85,93 +107,21 @@
 
 (def default-config-file "./snowflake-css.json")
 
-;; TODO: command line options
-;; --config-file
-;; --eject
-;; --scramble
-;; --rename
-;; --show-orphans
-;; --output-file
-; (defn- main [& js-args]
-;   (timbre/merge-config! {:output-fn format-log-msg})
-;   (let [args (js->clj (oget yargs "argv") :keywordize-keys true)
-;         config-file (get args :config-file default-config-file)
-;         config-file-options (try
-;                               (-> (.readFileSync fs config-file)
-;                                   js/JSON.parse
-;                                   (js->clj :keywordize-keys true))
-;                               (catch js/Object e nil))
-;         options (if config-file-options
-;                   config-file-options
-;                   args)
-;
-;
-;         css-files-glob (:css-files options)
-;         css-is-file? (.isFileSync fs css-files-glob)
-;         templates-path (:template-files options)
-;         templates-path-is-file? (.isFileSync fs templates-path)
-;
-;         ;; TODO: add an "overwrite-css-file" option that only works if they provide
-;         ;; a single CSS file as an option
-;
-;         ;; FIXME: if they pass in --output-css-file and it is the same thing as
-;         ;; -css-input then throw a warning
-;
-;         css-files (if css-is-file?
-;                     [css-search]
-;                     (glob-sync css-search))
-;         templates-files (if templates-path-is-file?
-;                           [templates-path]
-;                           (glob-sync templates-path))
-;
-;         _ (timbre/info "Reading CSS and template files for snowflake classes …")
-;         css-classes (reduce
-;                       (fn [acc file]
-;                         (assoc acc file (get-snowflake-classes-from-file file)))
-;                       {}
-;                       css-files)
-;         template-classes (reduce
-;                            (fn [acc file]
-;                              (assoc acc file (get-snowflake-classes-from-file file)))
-;                            {}
-;                            templates-files)
-;
-;         all-css-classes (apply set/union (vals css-classes))
-;         all-template-classes (apply set/union (vals template-classes))
-;         orphan-css-classes (set/difference all-css-classes all-template-classes)
-;         orphan-template-classes (set/difference all-template-classes all-css-classes)
-;
-;         first-css-file-contents (.readFileSync fs (first css-files) "utf8")]
-;
-;     (timbre/info "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-;     (timbre/info "Results:")
-;     (timbre/info "Found" (count all-css-classes) "snowflake classes in" (count css-classes) "CSS target files")
-;     (timbre/info "Found" (count all-template-classes) "snowflake classes in" (count template-classes) "template target files")
-;     (when-not (empty? orphan-template-classes)
-;       (timbre/warn (count orphan-template-classes) "template classes not found in CSS:" (vec orphan-template-classes)))
-;     (when-not (empty? orphan-css-classes)
-;       (timbre/warn (count orphan-css-classes) "classes not found in CSS:" (vec orphan-css-classes)))
-;
-;
-;     ;; TODO: log file size before / after here and the number of rules removed
-;     (when (:output-css-file args)
-;       (let [new-css-file (remove-classes-from-css first-css-file-contents orphan-css-classes)]
-;         (.writeFileSync fs (:output-css-file args) new-css-file "utf8")))))
-
 (defmulti run-command!
-  (fn [command-type _opts] command-type))
+  (fn [command-type _opts _args] command-type))
 
 (defmethod run-command! :build
-  [_command-type {:keys [css-snowflake-classes
-                         all-css-snowflake-classes
-                         template-snowflake-classes
-                         all-template-snowflake-classes]}]
-  (timbre/info "BUILD COMMAND go go!!")
-  (process-exit!))
+  [_command-type
+   {:keys [css-snowflake-classes
+           template-snowflake-classes
+           input-css-contents]}
+   {:keys [outputCSSFile]}]
+  (let [input-minus-null-flakes (remove-flakes-from-css input-css-contents template-snowflake-classes)]
+    (write-file-sync! outputCSSFile input-minus-null-flakes)
+    (process-exit!)))
 
 (defmethod run-command! :list
   [_command-type {:keys [css-snowflake-classes
-                         all-css-snowflake-classes
                          template-snowflake-classes
                          all-template-snowflake-classes]}]
   (timbre/info "LIST COMMAND go go!!")
@@ -179,16 +129,19 @@
 
 (defmethod run-command! :rename
   [_command-type {:keys [css-snowflake-classes
-                         all-css-snowflake-classes
                          template-snowflake-classes
                          all-template-snowflake-classes]}]
   (timbre/info "RENAME COMMAND go go!!")
   (process-exit!))
 
 (defmethod run-command! :default
-  [command-type _opts]
+  [command-type _opts _args]
   (timbre/error "Invalid command-type passed to run-command!" command-type)
   (process-exit! 1))
+
+;; TODO: input validation
+;; - inputCSSFile must exist
+;; - inputCSSFile must be a syntax-valid CSS File
 
 (defn- command-handler
   "this function runs for every yargs command handler
@@ -196,18 +149,17 @@
   then calls a corresponding defmethod for the 'command' type"
   [command-type js-args]
   (let [args (js->clj js-args :keywordize-keys true)
-        css-files (get-files-from-pattern-arg (:cssFiles args))
-        css-snowflake-classes (get-snowflake-classes-from-files css-files)
-        all-css-snowflake-classes (apply set/union (vals css-snowflake-classes))
+        input-css-contents (read-file-sync! (:inputCSSFile args))
+        css-snowflake-classes (get-snowflake-classes-from-str input-css-contents)
         template-files (get-files-from-pattern-arg (:templateFiles args))
-        template-snowflake-classes (get-snowflake-classes-from-files template-files)
-        all-template-snowflake-classes (apply set/union (vals template-snowflake-classes))]
-    (run-command! command-type {:css-files css-files
-                                :css-snowflake-classes css-snowflake-classes
-                                :all-css-snowflake-classes all-css-snowflake-classes
-                                :template-files template-files
-                                :template-snowflake-classes template-snowflake-classes
-                                :all-template-snowflake-classes all-template-snowflake-classes})))
+        template-files->snowflake-classes (get-snowflake-classes-from-files template-files)
+        template-snowflake-classes (apply set/union (vals template-files->snowflake-classes))]
+    (run-command!
+      command-type
+      {:css-snowflake-classes css-snowflake-classes
+       :input-css-contents input-css-contents
+       :template-snowflake-classes template-snowflake-classes}
+      args)))
 
 ;; -----------------------------------------------------------------------------
 ;; CLI Commands
@@ -221,6 +173,8 @@
 ;; - report   "Print a report of how many classes recognized in what files"
 ;; - scramble "Scramble snowflake hashes"
 
+;; "default" command (ie: no argument passed) should be "help" (show the options)
+
 (def js-build-command
   (js-obj
     "command" "build"
@@ -230,7 +184,7 @@
 
 (def js-rename-command
   (js-obj
-    "command" "rename"
+    "command" "rename <from> <to>"
     "describe" "Rename a snowflake class"
     "handler" (partial command-handler :rename)))
 
